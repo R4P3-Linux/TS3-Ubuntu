e74777500a5276fc8111c094853097da8f063906
R4P3-Linux <31107923+R4P3-Linux@users.noreply.github.com>
2017-08-17 21:44:17 -0400

    Clean Garbage
    Remove extra file

--- KneescR4P3.sh
+++ KneescR4P3.sh
@@ -11,14 +11,17 @@ if [ "${arch}" = "x86_64" ]; then
         sudo ./TeamSpeak3-Client-linux_amd64-3.1.6.run
         cd TeamSpeak3-Client-linux_amd64
         sudo ln -s $PWD/ts3client_runscript.sh /usr/local/bin/teamspeak
-        sudo rm -f ./TeamSpeak3-Client-linux_amd64-3.1.6.run
+        sudo rm -f ../TeamSpeak3-Client-linux_amd64-3.1.6.run
+        rm -f $0
+        
 else
         sudo wget http://dl.4players.de/ts/releases/3.1.6/TeamSpeak3-Client-linux_x86-3.1.6.run
         sudo chmod +x TeamSpeak3-Client-linux_x86-3.1.6.run
         sudo ./TeamSpeak3-Client-linux_x86-3.1.6.run
         cd TeamSpeak3-Client-linux_x86
         sudo ln -s $PWD/ts3client_runscript.sh /usr/local/bin/teamspeak
-        sudo rm -f ./TeamSpeak3-Client-linux_x86-3.1.6.run
+        sudo rm -f ../TeamSpeak3-Client-linux_x86-3.1.6.run
+        rm -f $0
 fi
 
 clear
